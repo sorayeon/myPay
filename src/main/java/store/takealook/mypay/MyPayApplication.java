1e811dcb367f804b577bc68df96e01abf44e17e7
sorayeon <jihwan77@gmail.com>
2023-12-27 00:34:16 +0900

    Init Project

---
+++ src/main/java/store/takealook/mypay/MyPayApplication.java
@@ -0,0 +1,13 @@
+package store.takealook.mypay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyPayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyPayApplication.class, args);
+    }
+
+}
